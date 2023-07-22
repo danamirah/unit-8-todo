d6241533ee85747985d107e257b350cbe4476c18
danamirah <vanaamirah@gmail.com>
2023-07-22 11:38:08 +0300

    Initial Commit

---
+++ unit 8 todo/unit_8_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  unit_8_todoApp.swift
+//  unit 8 todo
+//
+//  Created by Dana on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct unit_8_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
